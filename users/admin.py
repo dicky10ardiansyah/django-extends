c3306b004a1bd970a010c89382c812d4ca0ec5a5
Dicky Ardiansyah <eigerdlamborghini.ardiansyah@gmail.com>
2022-05-05 12:00:11 +0700

    django extends

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 from .models import NewUser
-admin.site.register(NewUser)
+from django.contrib.auth.admin import UserAdmin
+from django.forms import Textarea 
+
+class UserAdminConfig(UserAdmin):
+    
+    model = NewUser
+    search_fields = ('email', 'user_name', 'first_name')
+    list_filter = ('email', 'user_name', 'first_name', 'is_active', 'is_staff')
+    ordering = ('-start_date',)
+    list_display = ('email', 'user_name', 'first_name', 'is_staff', 'is_active')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'user_name', 'first_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        ('Personal', {'fields': ('about',)}),
+    )
+    formfield_overrides = {
+        NewUser.about: {'widget': Textarea(attrs={'rows':10, 'cols':40})},
+    }
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'user_name', 'first_name', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+
+admin.site.register(NewUser, UserAdminConfig)
